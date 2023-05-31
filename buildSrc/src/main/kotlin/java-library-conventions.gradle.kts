08b1ef04f40609f15a82e3bb05777a9ac797c489
MartinDai <xiaomingys@gmail.com>
2023-05-31 11:37:08 +0800

    仓库配置调整

--- buildSrc/src/main/kotlin/java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/java-library-conventions.gradle.kts
@@ -3,6 +3,11 @@ plugins {
 }
 
 repositories {
+    //首先去本地仓库找
+    mavenLocal()
+    //然后去阿里仓库找
+    maven("https://maven.aliyun.com/nexus/content/groups/public/")
+    //最后从maven中央仓库找
     mavenCentral()
 }
 
